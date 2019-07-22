e820d0729062ab30e8297fc6c2e77e56d39c267a
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2019-07-22 15:17:15 +0800

    [ch10] add a toggle button

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,22 +1,41 @@
 <template>
-  <input
-    :type="text"
-    :name="name"
-    :id="name"
-    class="form-control"
-  >
+  <div>
+    <input
+      :type="inputType"
+      :name="name"
+      :id="name"
+      class="form-control"
+    >
+    <a
+      href="#"
+      v-if="type === 'password'"
+      @click.prevent="toggleVisibility"
+    >Reveal password</a>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      inputType: this.type
+    }
+  },
+
   props: {
     name: {
       type: String,
       required: true
     },
-    text: {
+    type: {
       type: String,
       required: false
+    }
+  },
+
+  methods: {
+    toggleVisibility () {
+      this.inputType = this.inputType === 'password' ? 'text': 'password'
     }
   }
 }
